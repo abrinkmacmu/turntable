ba5bc00cf1d3428d00803c1e715964b85a8ad723
abrinkmacmu <abrinkma@andrew.cmu.edu>
2016-03-26 16:06:50 -0400

    refactored run_turntable

--- src/run_turntable.cpp
+++ src/run_turntable.cpp
@@ -7,64 +7,107 @@
 
 #include "turntable/saveImages.h"
 
-int main(int argc, char **argv)
-{
- 
-  ros::init(argc, argv, "talker");
+class turntable_class{
+public:
   ros::NodeHandle n;
-  ros::Publisher LA_pub = n.advertise<std_msgs::Int16>("LA_command_1", 1);
-  ros::Publisher Dyn_pub = n.advertise<std_msgs::Float64>("turntable_controller/command", 1);
+  ros::Publisher LA_pub;
+  ros::Publisher Dyn_pub;
 
-  ros::ServiceClient save_images_client = n.serviceClient<turntable::saveImages>("save_images");
-
-  ros::Rate loop_rate(10);
-  
   std_msgs::Int16 LA_cmd_sm;
   std_msgs::Float64 Dyn_cmd_sm;
-  float LA_min = 150; 
-  float LA_max = 850;
-  float Dyn_min = -135;
-  float Dyn_max = 135;
 
-  float LA_cmd = LA_min;
-  float Dyn_cmd= Dyn_min;
+  float LA_commands[4];
+  int LA_count;
 
-  float LA_n = 3;
-  float Dyn_n = 20;
+  float Dyn_min;
+  float Dyn_max;
+  float Dyn_cmd;
+  float Dyn_n;
+  float Dyn_delta;
 
-  float LA_delta = (LA_max - LA_min)/ LA_n;
-  float Dyn_delta = (Dyn_max - Dyn_min)/Dyn_n;
-  while (ros::ok())
-  {
+  turntable_class(){
+    LA_pub = n.advertise<std_msgs::Int16>("LA_command_1", 1);
+    Dyn_pub = n.advertise<std_msgs::Float64>("turntable_controller/command", 1);
+    
+    Dyn_min = -135;
+    Dyn_max = 135;
+    Dyn_cmd= Dyn_min;
+    Dyn_n = 5;
+    Dyn_delta = (Dyn_max - Dyn_min)/Dyn_n;
+
+    LA_commands[0] = 150;
+    LA_commands[1] = 300;
+    LA_commands[2] = 600;
+    LA_commands[3] = 900;
+    LA_count = 0;
+    LA_cmd_sm.data = LA_commands[LA_count];
+    LA_pub.publish(LA_cmd_sm);
+    ros::Duration(4.0).sleep();
+
+  }
+  int update_turntable(){
     Dyn_cmd += Dyn_delta;
+    float additional_delay = 0;
+    int reset = 0;
+    if(Dyn_cmd > Dyn_max){
+      Dyn_cmd = Dyn_min;
+      additional_delay = 5.0;
+      reset = 1;
+    }
+
     Dyn_cmd_sm.data = Dyn_cmd/180.0*M_PI;
     std::cout<< Dyn_cmd_sm.data<<std::endl;
     Dyn_pub.publish(Dyn_cmd_sm);
-    ros::Duration(3.0).sleep();
+    std::cout << "waiting to settle...\n";
+    ros::Duration(3.0+additional_delay).sleep();
+    std::cout << "turntable has settled\n";
+    return reset;
+  }
+  void update_kinect_angle(){
+    LA_count++;
+    LA_cmd_sm.data = LA_commands[LA_count%4];
+    std::cout << "LA count: "<< LA_count << ", mod 4 : "<< LA_count%4<<std::endl;
 
-    if(Dyn_cmd > Dyn_max){
-      Dyn_cmd = Dyn_min;
+    LA_pub.publish(LA_cmd_sm);
+    ros::Duration(4.0).sleep();   
+  }
 
-      LA_cmd += LA_delta;
-      LA_cmd_sm.data = LA_cmd;
+  float get_current_tt_angle(){
+    return Dyn_cmd/180.0*M_PI;
+  }
+  float get_current_kinect_angle(){
+    return 15.0*float(LA_count % 4);
+  }
 
-      LA_pub.publish(LA_cmd_sm);
-      ros::Duration(2.0).sleep();
-      
+};
 
-      
+int main(int argc, char **argv){
+ 
+  ros::init(argc, argv, "turntable");
+  ros::NodeHandle n;
+  ros::ServiceClient save_images_client = n.serviceClient<turntable::saveImages>("save_images");
+  turntable_class tt;
+  ros::Rate loop_rate(10);
+  int reset_angle;
+  turntable::saveImages srv;
 
-      if(LA_cmd > LA_max){
-        LA_cmd = LA_min;
-      }
+  float current_tt_angle, current_kinect_angle, image_number;
+  current_kinect_angle = tt.get_current_kinect_angle();
+
+  while (ros::ok())
+  {
+    
+    reset_angle = tt.update_turntable();
+    current_tt_angle = tt.get_current_tt_angle();
+    if(reset_angle == 1){
+      tt.update_kinect_angle();
     }
-    turntable::saveImages srv;
+      
     srv.request.item_number = 1;
     srv.request.angle = 30;
     srv.request.count = 10;
     bool res = save_images_client.call(srv);
-    std::cout << "response is: "<< res<< std::endl;
-    std::cout << "Dyn:"<< Dyn_cmd<<"  LA: "<<LA_cmd<<std::endl;
+    std::cout << "response is: "<< srv.response.status<< std::endl;
     ros::spinOnce();
     loop_rate.sleep();
   }
